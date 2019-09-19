6369ac6508f658bb785e87be0ceceb3bfb35eca2
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2019-09-19 17:30:40 +0000

    Evitar que Odoo crea que se esta guardando un SVG ¯\_(ツ)_/¯

--- models/account.py
+++ models/account.py
@@ -176,7 +176,7 @@ class AccountInvoice(models.Model):
                 if factura.journal_id.tipo_documento_fel > 1:
                     DAPreimpreso.text = factura.factura_original_id.numero_fel
 
-                xmls = etree.tostring(DocElectronico, xml_declaration=True, encoding="UTF-8")
+                xmls = etree.tostring(DocElectronico, encoding="UTF-8")
                 logging.warn(xmls)
                 datos = base64.b64encode(b" "+xmls)
                 factura.documento_xml_fel = datos
@@ -197,7 +197,7 @@ class AccountInvoice(models.Model):
                 resultado = resultado.replace("&", "&amp;")
                 logging.warn(resultado)
                 datos = base64.b64encode(b" "+(resultado.encode("utf-8")))
-                factura.resultado_xml_fel = resultado
+                factura.resultado_xml_fel = datos
                 factura.resultado_xml_fel_name = "resultado_xml_fel.xml"
 
                 resultadoXML = etree.XML(resultado)
