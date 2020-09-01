4b0265be3e0961f7cf1ad46d82d4ce0d2791d6cf
rocketgithub <rodrigo@solucionesprisma.com>
2020-09-01 08:24:52 -0600

    Poder leer resultado de la versión 12 de FEL

--- models/account.py
+++ models/account.py
@@ -238,16 +238,16 @@ class AccountInvoice(models.Model):
                     factura.resultado_xml_fel_name = "resultado_xml_fel.xml"
 
                     resultadoXML = etree.XML(resultado.encode("utf-8"))
-                    numero_autorizacion = resultadoXML.find(".//{http://www.sat.gob.gt/dte/fel/0.1.0}NumeroAutorizacion")
-                    nombre_receptor = resultadoXML.find(".//{http://www.sat.gob.gt/dte/fel/0.1.0}Receptor")
-                    direccion_receptor = resultadoXML.find(".//{http://www.sat.gob.gt/dte/fel/0.1.0}DireccionReceptor")
+                    numero_autorizacion = resultadoXML.xpath("//*[local-name() = 'NumeroAutorizacion']")[0]
+                    nombre_receptor = resultadoXML.xpath("//*[local-name() = 'Receptor']")[0]
+                    direccion_receptor = resultadoXML.xpath("//*[local-name() = 'DireccionReceptor']")
 
                     factura.firma_fel = numero_autorizacion.text
                     factura.name = numero_autorizacion.get("Serie")+"-"+numero_autorizacion.get("Numero")
                     factura.serie_fel = numero_autorizacion.get("Serie")
                     factura.numero_fel = numero_autorizacion.get("Numero")
-                    factura.nombre_cliente_fel = html.unescape(nombre_receptor.get("NombreReceptor"))
-                    factura.direccion_cliente_fel = html.unescape(direccion_receptor.find("{http://www.sat.gob.gt/dte/fel/0.1.0}Direccion").text) if direccion_receptor else ''
+                    factura.nombre_cliente_fel = nombre_receptor.get("NombreReceptor")
+                    factura.direccion_cliente_fel = direccion_receptor[0].xpath("//*[local-name() = 'Direccion']")[0].text if len(direccion_receptor) > 0 else ''
                 else:
                     raise UserError("Error en Guatefacturas: "+resultado)
 
